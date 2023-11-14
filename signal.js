9896c175b93bf646a019042e787b40ca69354209
Francesco Bedussi <francesco.bedussi@abstract.it>
2023-11-14 23:41:08 +0100

    init

---
+++ signal.js
@@ -0,0 +1,23 @@
+export function createSignal(initialValue) {
+  let _value = initialValue;
+  let subscribers = [];
+
+  function notify() {
+    for (let subscriber of subscribers) {
+      subscriber(_value);
+    }
+  }
+
+  return {
+    get value() {
+      return _value;
+    },
+    set value(v) {
+      _value = v;
+      notify();
+    },
+    subscribe: (subscriber) => {
+      subscribers.push(subscriber);
+    }
+  };
+}
